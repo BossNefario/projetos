1954905637023b9a3a96d6f863310304cf696307
BossNefario <preischardt@hotmail.com>
2023-06-27 13:23:44 -0300

    Modulo 5 Semana1

--- analista_teste/modulo3_tester_javascript/semana1/atividades1.js
+++ analista_teste/modulo3_tester_javascript/semana1/atividades1.js
@@ -8,8 +8,11 @@ let media = (nota1 + nota2) / 2;
 // Exibição da nota e resultado
 console.log("A nota obtida foi:", media.toFixed(2)); // exibe a nota com duas casas decimais
 
+
 if (media >= 6) {
   console.log("Aprovado"); // imprimir o resultado "Aprovado"
 } else {
   console.log("Reprovado"); // imprimir o resultado "Reprovado"
 }
+
+// console.log(media >= 6 ? "Aprovado" : "Reprovado");
