c22ef382003977f131f56a5d1dfdf2f139490447
BossNefario <preischardt@hotmail.com>
2023-08-19 06:59:46 -0300

    Semana 03

--- python/modulo6_curso_python/semana01/rest_api/views.py
+++ python/modulo6_curso_python/semana01/rest_api/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 from reserva.models import Reserva
 from rest_api.serializers import AgendamentoModelSerializer, PetAgendamentoSerializer, DataAgendamentoSerializer
@@ -12,15 +14,20 @@ from rest_api.serializers import AgendamentoModelSerializer, PetAgendamentoSeria
 class AgendamentoModelViewSet(ModelViewSet):
     queryset = Reserva.objects.all()
     serializer_class = AgendamentoModelSerializer
-    
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+        
 class AgendamentoDataViewSet(ModelViewSet):
     queryset = Reserva.objects.all()
     serializer_class = DataAgendamentoSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class AgendamentoPetViewSet(ModelViewSet):
     queryset = Reserva.objects.all()
     serializer_class = PetAgendamentoSerializer
-
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
 @api_view(['GET', 'POST'])
 def hello_world(request):
